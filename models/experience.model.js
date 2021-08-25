6f66945fe432f62c383c7ace31567d3eba7509dc
juan_j1501 <juanjose1514@gmail.com>
2021-08-24 22:32:42 -0500

    (Hot-Fix): JUan José no sabe escribir responsabilities

--- models/experience.model.js
+++ models/experience.model.js
@@ -7,7 +7,7 @@ const experienceInfoSchema = new Schema({
     company_start_date: { type: Date, required: true }, //fecha inicio
     company_end_date: { type: Date},  //fecha fin (no requerida por si la persona continúa)
     city: { type: String,trim: true },
-    responsabilites: { type: Array, of: String },
+    responsabilities: { type: Array, of: String },
     achievements: { type: Array, of: String },
 });
 
